527cf34af23cbcbd65f01766893db19e442eefca
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-22 15:50:53 +0100

    Added configuration for Typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,17 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
-    jest: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 13,
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
-    "no-param-reassign": "off",
-    "no-plusplus": "off",
-    "no-restricted-syntax": "off",
-    "consistent-return": "off",
-    "no-debugger": "off",
-    "no-console": "off",
   },
 };
