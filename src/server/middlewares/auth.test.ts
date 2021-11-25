b5f7cf661dea740734312673350823b526313e49
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-25 10:10:00 +0100

    Fixed test auth

---
+++ src/server/middlewares/auth.test.ts
@@ -0,0 +1,11 @@
+// import auth from "./auth";
+
+// import jwt from "jsonwebtoken";
+
+// describe("Given an auth fucntion", () => {
+//   describe("When it receives", () => {
+//     test("Then", () => {
+
+//     })
+//   })
+// })
