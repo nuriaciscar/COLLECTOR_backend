2cfbabc1dbbcbf3ef8120889cb15ea2bc55166b4
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-26 13:58:20 +0100

    Added verifyImage function to verify owner

---
+++ src/server/middlewares/verifyImage.ts
@@ -0,0 +1,34 @@
+import chalk from "chalk";
+import Debug from "debug";
+import { NextFunction } from "express";
+import Image from "../../database/models/image";
+import { RequestAuth } from "../../utils/mocks/mockFunctions";
+
+const debug = Debug("collector:middleware:verifyImage");
+
+const verifyImage = async (
+  req: RequestAuth,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { idImage } = req.params;
+    const image = await Image.findById(idImage);
+    if (image.owner.includes(req.idUser)) {
+      return next();
+    }
+    const error: {
+      code?: number;
+      message: string;
+    } = new Error("Not allowed");
+    error.code = 401;
+    next(error);
+  } catch (error) {
+    error.message = "Image not found";
+    debug(chalk.redBright("Image not found"));
+    error.code = 400;
+    next(error);
+  }
+};
+
+export default verifyImage;
--- src/server/routes/imageRoutes.ts
+++ src/server/routes/imageRoutes.ts
@@ -10,13 +10,14 @@ import {
 import upload from "../middlewares/uploadLocal";
 import firebase from "../middlewares/firebase";
 import auth from "../middlewares/auth";
+import verifyImage from "../middlewares/verifyImage";
 
 const router = express.Router();
 
 router.get("/:idImage", getImage);
 router.get("/", getImages);
-router.patch("/:idImage", auth, updateImage);
-router.delete("/:idImage", auth, deleteImage);
+router.patch("/:idImage", auth, verifyImage, updateImage);
+router.delete("/:idImage", auth, verifyImage, deleteImage);
 router.post("/addImage", auth, upload.single("image"), firebase, addImage);
 
 export default router;
