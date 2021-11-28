4cf2ef2fb3618ee8337e0fbe083ec81fe8682c50
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-28 12:36:11 +0100

    Added validationError on error handlers and refactorized error.test

--- src/server/middlewares/error.ts
+++ src/server/middlewares/error.ts
@@ -1,25 +1,31 @@
-import * as express from 'express';
+import * as express from "express";
 
-import Debug from 'debug';
+import Debug from "debug";
+import { ValidationError } from "express-validation";
+import { IError } from "../../utils/mocks/mockFunctions";
 
-const debug = Debug('collector:server-errors');
-const chalk = require('chalk');
+const debug = Debug("collector:server-errors");
+const chalk = require("chalk");
 
 export const notFoundErrorHandler = (req: any, res: express.Response) => {
   res.status(404).json({ error: "Endpoint not found" });
 };
 
 export const errorHandler = (
-  error,
+  error: IError,
   req: express.Request,
   res: express.Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: express.NextFunction
 ) => {
+  if (error.statusCode === 400) {
+    debug(chalk.red("Not valid format request"));
+    error.code = 400;
+    error.message = "Not valid request";
+  }
   debug(chalk.red("An error has occurred: ", error.message));
-
   const message = error.code ? error.message : "All broken";
   res.status(error.code || 500).json({ error: message });
 };
 
-export default {notFoundErrorHandler, errorHandler}
+export default { notFoundErrorHandler, errorHandler };
