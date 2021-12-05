67ae42dd2e07ae0478a656ea585330449d7565b2
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-12-05 13:47:56 +0100

    Fixed joi validation on collection routes

--- src/server/routes/imageRoutes.ts
+++ src/server/routes/imageRoutes.ts
@@ -8,7 +8,7 @@ import {
 } from "../controllers/imageController";
 import upload from "../middlewares/uploadLocal";
 import firebase from "../middlewares/firebase";
-// import auth from "../middlewares/auth";
+import auth from "../middlewares/auth";
 import verifyImage from "../middlewares/verifyImage";
 
 const router = express.Router();
@@ -16,7 +16,7 @@ const router = express.Router();
 router.get("/:idImage", getImage);
 router.get("/", getImages);
 router.patch("/:idImage", verifyImage, updateImage);
-router.delete("/:idImage", verifyImage, deleteImage);
-router.post("/addImage", upload.array("image"), firebase, addImage);
+router.delete("/:idImage", auth, verifyImage, deleteImage);
+router.post("/addImage", auth, upload.array("image"), firebase, addImage);
 
 export default router;
